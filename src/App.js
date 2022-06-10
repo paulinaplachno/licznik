4303c3c8aab4087e308c9e62eea5c08a629bf07c
Paulina Płachno <paulina1plachno@gmail.comgit config -- global user.namecleargit configgit config --global user.email paulina1plachno@gmail.com>
2022-06-10 11:56:36 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  const increase = () => {
+    setCounter(prevState => prevState + 1);
+  };
+  const decrease = () => {
+    setCounter(prevState =>prevState - 1);
+  };
+  const clear = () => {
+    setCounter(0);
+  };
+
+return (
+  <div>
+    <h2>Stan licznika: {counter}</h2>
+    <br />
+    <button onClick={increase} class="glow-on-hover" type="button"> Zwiększ o jeden</button>
+    <br />
+    <button onClick={clear} class="glow-on-hover" type="button"> Zeruj</button>
+    <br />
+    <button onClick={decrease} class="glow-on-hover" type="button"> Zmniejsz o jeden </button>
+    <br />
+  </div>
+);
+};
+
+export default Counter;
